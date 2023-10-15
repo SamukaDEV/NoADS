bc0617dfaf05ceb9c3a56a6fad2ee453f81ef900
Samuel Torres <absfregs@gmail.com>
2023-10-15 10:25:10 -0300

    Update content.js

--- js/content.js
+++ js/content.js
@@ -13,6 +13,9 @@ let _tmr_sda = setInterval(() => {
     const uol_botside = document.querySelector('#bp-footer > div > div.col-md-9.hidden-xs.ads__wrapper___dTcn')
     const uol_fbook = document.querySelector('#bp-footer > div > div.col-xs-2.col-sm-2.col-md-2.col-lg-2.hidden-xs > div:nth-child(2) > div')
 
+    const yt_ads = document.querySelector('.yt-simple-endpoint.style-scope.ytd-action-companion-ad-renderer')
+    const yt_ads2 = document.querySelector('.style-scope.ytd-in-feed-ad-layout-renderer')
+
     if (overlay_slot) overlay_slot.remove()
     if (skip_button) skip_button.click()
     if (progress_dot) progress_dot.remove()
@@ -20,4 +23,8 @@ let _tmr_sda = setInterval(() => {
     if (uol_topside) uol_topside.remove()
     if (uol_botside) uol_botside.remove()
     if (uol_fbook) uol_fbook.remove()
+
+    if (yt_ads) yt_ads.remove()
+    if (yt_ads2) yt_ads2.remove()
+
 }, 100)
