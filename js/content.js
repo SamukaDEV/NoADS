269c22dec072f7741275b7db9e55387c99849a69
Samuel Torres <absfregs@gmail.com>
2023-12-30 11:47:18 -0300

    Backup

--- js/content.js
+++ js/content.js
@@ -1,9 +1,17 @@
-const yt_div = document.querySelector('.ytp-left-controls')
-const div = document.createElement('div')
-div.classList.add('ytp-time-display')
-div.innerHTML = '<span>Active</span>'
-yt_div.append(div)
+const cube_icon = /* html */`
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" 
+    width="20" height="20" 
+    x="0" y="0" 
+    viewBox="0 0 20 20" 
+    style="enable-background:new 0 0 20 20" 
+    xml:space="preserve" class="" style="position: relative;top: 5px;width: 20px;"><g><path fill="#656565" d="M469.882 132.446v247.031L256 503.031V255.923z" opacity="1" data-original="#1367f7" class=""></path><path fill="#c5c5c5" d="M256 255.923v247.108L42.118 379.477V132.446z" opacity="1" data-original="#7eb4fa" class=""></path><path fill="#f3f3f3" d="M469.882 132.446 256 255.923 42.118 132.446 256 8.969z" opacity="1" data-original="#c6e2f8" class=""></path></g></svg>`
 
+// const yt_div = document.querySelector('.ytp-left-controls')
+// const div = document.createElement('div')
+// div.classList.add('ytp-time-display')
+// div.innerHTML = /* html */`<span>${cube_icon}Active</span>`
+// yt_div.append(div)
+console.log('Loaded')
 let _tmr_sda = setInterval(() => {
     const overlay_slot = document.querySelector('.ytp-ad-overlay-slot')
     const skip_button = document.querySelector('.ytp-ad-skip-button')
